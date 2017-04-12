833a7dbb5d16a60dcd9c658afef768e77e821e8c
Juan Carlos <juanjcsr@gmail.com>
2017-04-12 14:22:23 -0500

    scrapper de ids

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/headzoo/surf/jar"
@@ -9,27 +17,68 @@ import (
 )
 
 func main() {
-	// doc, err := goquery.NewDocument("https://compranet.funcionpublica.gob.mx/esop/toolkit/opportunity/opportunityList.do")
-
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// doc.Find("body").Each(func(index int, item *goquery.Selection) {
-
-	// 	fmt.Printf("item %s", item.Text())
-	// })
-
-	bow := surf.NewBrowser()
-	bow.SetCookieJar(jar.NewMemoryCookies())
-	err := bow.Open("https://compranet.funcionpublica.gob.mx/esop/guest/go/public/opportunity/past?locale=es_MX")
+	idFile, err := os.OpenFile("ids", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
 	if err != nil {
 		panic(err)
 	}
+
+	defer idFile.Close()
+
+	bow := surf.NewBrowser()
+	bow.SetCookieJar(jar.NewMemoryCookies())
+	ferr := bow.Open("https://compranet.funcionpublica.gob.mx/esop/guest/go/public/opportunity/past?locale=es_MX")
+	if ferr != nil {
+		panic(ferr)
+	}
 	fmt.Println(bow.Title())
 	// fmt.Printf("%+v", bow.Dom())
-	bow.Dom().Each(func(i int, s *goquery.Selection) {
-		fmt.Println(s.Text())
-	})
+	// bow.Dom().Each(func(i int, s *goquery.Selection) {
+	// 	fmt.Println(s.Text())
+	// })
+	// TODO: revisar REGEXP
+	idRegexp, _ := regexp.Compile("([0-9]+)', ")
+	totalPaginasRegexp, _ := regexp.Compile("([0-9]+)")
+	totalCountRaw := strings.TrimSpace(bow.Find("div#allButtons_nav1").Text())
+	log.SetOutput(os.Stdout)
+	// fmt.Println(totalPaginasRegexp.MatchString(totalPaginas))
+	// fmt.Println(totalPaginasRegexp.FindAllString(totalPaginas, -1))
+	totalCount, err := strconv.Atoi(totalPaginasRegexp.FindAllString(totalCountRaw, -1)[0])
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Printf("TOTAL DOCS: %d\n", totalCount)
 
+	for currentPage := 1; currentPage < 53558; currentPage++ {
+		log.Printf("PAGINA %d", currentPage)
+
+		bow.Dom().Find("td.col__fixed_ a").Each(func(i int, s *goquery.Selection) {
+			// fmt.Println(s.Attr("onclick"))
+			idText, _ := s.Attr("onclick") // JS attr con el ID
+			id := idRegexp.FindStringSubmatch(idText)
+			log.Println(id[1]) //TODO: fix regexp == finds the ID to navigate
+			if _, err = idFile.WriteString(id[1] + "\n"); err != nil {
+				log.Printf("could not print: %v", err)
+			}
+		})
+		frm, err := bow.Form("#cntList form") //encuentra la forma de la tabla
+		if err != nil {
+			log.Fatalf("could not find the form: %v", err)
+		}
+		currentPage++
+
+		frm.Input("listManager.pagerComponent.page", strconv.Itoa(currentPage))
+		sleepTimer := randomTimer()
+		log.Printf("durmiendo por: %d segundos", sleepTimer)
+		time.Sleep(time.Duration(sleepTimer) * time.Second)
+		if frm.Submit() != nil {
+			panic(err)
+		}
+
+	}
+
+}
+
+func randomTimer() int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(6-1) + 1
 }
