7bee1521a2a649edc4c8bf6dcdcb6ccedbe0e191
Juan Carlos <juanjcsr@gmail.com>
2017-04-21 19:01:19 -0500

    ultimos toques

--- scraper/main.go
+++ scraper/main.go
@@ -10,6 +10,12 @@ import (
 
 	"time"
 
+	"strconv"
+
+	_ "net/http/pprof"
+
+	"flag"
+
 	"github.com/headzoo/surf/agent"
 	"github.com/headzoo/surf/jar"
 	"gopkg.in/headzoo/surf.v1"
@@ -29,7 +35,7 @@ func getExpedienteData(id int, expURL string, browser *browser.Browser) (*Expedi
 	return exp, nil
 }
 
-func openOutputFile(name string) *os.File {
+func openFile(name string) *os.File {
 	file, err := os.OpenFile(name, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
 	if err != nil {
 		log.Fatalf("could not open file: %v", err)
@@ -37,12 +43,12 @@ func openOutputFile(name string) *os.File {
 	return file
 }
 
-func startScrapping(startExp, endExp int, outFile *os.File, htmlDir string, browser *browser.Browser) error {
+func startScrapping(startExp, endExp int, htmlDir string, browser *browser.Browser, outFile *os.File, logFile *os.File) (int, error) {
 	if endExp < startExp {
-		return fmt.Errorf("el valor de inicio debe ser menor al del final")
+		return 0, fmt.Errorf("el valor de inicio debe ser menor al del final")
 	}
 
-	for idCompranet := startExp; idCompranet < endExp; idCompranet++ {
+	for idCompranet := startExp; idCompranet <= endExp; idCompranet++ {
 		expedienteURL := fmt.Sprintf("https://compranet.funcionpublica.gob.mx/esop/toolkit/opportunity/opportunityDetail.do?opportunityId=%d&oppList=PAST", idCompranet)
 		exp, err := getExpedienteData(idCompranet, expedienteURL, browser)
 		if err != nil {
@@ -57,6 +63,17 @@ func startScrapping(startExp, endExp int, outFile *os.File, htmlDir string, brow
 			if err != nil {
 				log.Printf("no puedo escribir el archivo HTML: %v", err)
 			}
+			writeLastToLog(logFile, idCompranet)
+			time.Sleep(3 * time.Second)
+			log.Printf("ended exp: %d", idCompranet)
+		}
+
+		if idCompranet%2 == 0 {
+			browser, err = getNewBrowserWithSession("https://compranet.funcionpublica.gob.mx/esop/guest/go/public/opportunity/past?locale=es_MX")
+			if err != nil {
+				return idCompranet, fmt.Errorf("no puedo abrir la pagina: %v", err)
+			}
+			log.Println("getting new session")
 		}
 
 		// browser, _ = getNewBrowserWithSession("https://compranet.funcionpublica.gob.mx/esop/guest/go/public/opportunity/past?locale=es_MX")
@@ -66,12 +83,38 @@ func startScrapping(startExp, endExp int, outFile *os.File, htmlDir string, brow
 
 	}
 
-	return nil
+	return endExp, nil
 }
 
 func main() {
-	outputFile := openOutputFile("data.json")
+
+	// app := cli.NewApp()
+
+	// app.Flags = []cli.Flag {
+	// 	cli.StringFlag{
+	// 		Name: "outfile"
+	// 	}
+	// }
+
+	// app.Name = "compranet expedientes scraper"
+	// app.Usage = "obtiene los expedientes de compranet"
+	// app.Action = func(c *cli.Context) error {
+	// 	fmt.Println("running from CLI")
+	// 	return nil
+	// }
+
+	// app.Run(os.Args)
+	var startVal int
+	var endVal int
+	flag.IntVar(&startVal, "inicio", 1, "valor de expediente inicial")
+	flag.IntVar(&endVal, "fin", 1209999, "valor de expediente final")
+	flag.Parse()
+
+	outputFile := openFile("data.json")
 	defer outputFile.Close()
+
+	logFile := openFile("log.out")
+	defer logFile.Close()
 
 	rawHTMLFolder := "htmls"
 	os.Mkdir(rawHTMLFolder, 0777)
@@ -80,17 +123,26 @@ func main() {
 	if err != nil {
 		log.Fatalf("no puedo abrir la pagina: %v", err)
 	}
+	lastRead := startVal
 
-	startScrapping(1, 5, outputFile, rawHTMLFolder, browser)
+	for {
+		lastRead, err := startScrapping(lastRead, endVal, rawHTMLFolder, browser, outputFile, logFile)
+		if err != nil {
+			log.Printf("could not continue scraping, %v, sleeping for 60 seconds", err)
+			time.Sleep(60 * time.Second)
+			browser, _ = getNewBrowserWithSession("https://compranet.funcionpublica.gob.mx/esop/guest/go/public/opportunity/past?locale=es_MX")
+			log.Printf("restarting from %d", lastRead)
+		}
+		if lastRead == endVal {
+			break
+		}
+
+	}
 
 }
-
-func openFile(fileName string) *os.File {
-	file, err := os.Open(fileName)
-	if err != nil {
-		log.Fatalf("could not open file: %s", err.Error())
-	}
-	return file
+func writeLastToLog(file *os.File, last int) {
+	s := strconv.Itoa(last)
+	file.WriteString(s + "\n")
 }
 
 func getNewBrowserWithSession(url string) (bow *browser.Browser, err error) {
