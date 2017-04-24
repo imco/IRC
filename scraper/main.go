d927715c8ae3b10009c88234fa3006147db23ed5
Juan Carlos <juanjcsr@gmail.com>
2017-04-24 16:25:11 -0500

    en caso de falta de datos, reiniciar sesion y dormir 10 sec

--- scraper/main.go
+++ scraper/main.go
@@ -21,6 +21,8 @@ import (
 	"gopkg.in/headzoo/surf.v1"
 )
 
+// ScrapErr es un wrapper de error para controlar casos donde
+// Compranet esté abajo y no funcione
 type ScrapErr struct {
 	remoteError bool
 	msg         string
@@ -33,12 +35,12 @@ func (s *ScrapErr) Error() string {
 // getExpedienteData accede a la URL del expediente dentro de CompraNet. Regresa un Expediente
 // con todos los elementos de la página que no son tablas
 func getExpedienteData(id int, expURL string, browser *browser.Browser) (*Expediente, error) {
-	if err := browser.Open(expURL); err != nil {
+	if err := browser.Open(expURL); err != nil { //intentar abrir la URL del expediente, regresar custom error
 		errMsg := fmt.Sprintf("could not open %s, error: %v", expURL, err)
 		return nil, &ScrapErr{true, errMsg}
 	}
 	qnaRaw := browser.Find("#cntDetail .form_container ul li") //Elementos que NO son tablas
-	exp, err := GetNewExpediente(qnaRaw)
+	exp, err := GetNewExpediente(qnaRaw)                       //Intenta obtener los datos del expediente
 	if err != nil {
 		return nil, fmt.Errorf("no data for %d", id)
 	}
@@ -62,18 +64,18 @@ func startScrapping(startExp, endExp int, htmlDir string, browser *browser.Brows
 	if endExp < startExp {
 		return 0, fmt.Errorf("el valor de inicio debe ser menor al del final")
 	}
-
+	newSession := false
 	// loop principal de scraping
 	for idCompranet := startExp; idCompranet <= endExp; idCompranet++ {
 		expedienteURL := fmt.Sprintf("https://compranet.funcionpublica.gob.mx/esop/toolkit/opportunity/opportunityDetail.do?opportunityId=%d&oppList=PAST", idCompranet)
 		exp, err := getExpedienteData(idCompranet, expedienteURL, browser)
-		if err != nil {
-
-			if _, ok := err.(*ScrapErr); ok {
+		if err != nil { //En caso que falle compranet por algun motivo
+			if _, ok := err.(*ScrapErr); ok { //revisar que el tipo del error sea uno nuestro
 				// log.Printf("remote error: %v", sc)
 				return idCompranet, err
 			}
-			log.Printf("error: %v", err) //No se obtuvieron datos
+			newSession = true
+			log.Printf("error: %v", err) //No se obtuvieron datos para ese id
 			// return idCompranet, err
 		} else {
 			// toca obtener las tablas y datos faltantes
@@ -87,18 +89,20 @@ func startScrapping(startExp, endExp int, htmlDir string, browser *browser.Brows
 				log.Printf("no puedo escribir el archivo HTML: %v", err)
 			}
 			writeLastToLog(logFile, idCompranet) //y llevamos registro de lo último que se pudo guardar
-			time.Sleep(2 * time.Second)          //TODO: encontrar intervalo
+			time.Sleep(2 * time.Second)          //TODO: encontrar intervalo random para que compranet no explote
 			log.Printf("ended exp: %d", idCompranet)
 
-			// reiniciar browser despues de 100 requests
-			if idCompranet%100 == 0 {
-				log.Println("getting new session")
-				browser, err = getNewBrowserWithSession("https://compranet.funcionpublica.gob.mx/esop/guest/go/public/opportunity/past?locale=es_MX")
-				if err != nil {
-					return idCompranet, fmt.Errorf("no puedo abrir la pagina: %v", err)
-				}
-
+		}
+		// reiniciar browser despues de 100 requests
+		if idCompranet%100 == 0 || newSession {
+			log.Println("getting new session")
+			time.Sleep(10 * time.Second)
+			browser, err = getNewBrowserWithSession("https://compranet.funcionpublica.gob.mx/esop/guest/go/public/opportunity/past?locale=es_MX")
+			if err != nil {
+				return idCompranet, fmt.Errorf("no puedo abrir la pagina: %v", err)
 			}
+			newSession = false
+
 		}
 
 	}
@@ -108,29 +112,16 @@ func startScrapping(startExp, endExp int, htmlDir string, browser *browser.Brows
 
 func main() {
 
-	// app := cli.NewApp()
+	//TODO: utlizar urfave/cli para manejar flag y línea de comandos
 
-	// app.Flags = []cli.Flag {
-	// 	cli.StringFlag{
-	// 		Name: "outfile",
-
-	// 	}
-	// }
-
-	// app.Name = "compranet expedientes scraper"
-	// app.Usage = "obtiene los expedientes de compranet"
-	// app.Action = func(c *cli.Context) error {
-	// 	fmt.Println("running from CLI")
-	// 	return nil
-	// }
-
-	// app.Run(os.Args)
+	//definicion de flags y de valores iniciales
 	var startVal int
 	var endVal int
 	flag.IntVar(&startVal, "inicio", 1, "valor de expediente inicial")
 	flag.IntVar(&endVal, "fin", 1209999, "valor de expediente final")
 	flag.Parse()
 
+	//
 	outputFile := openFile("data.json")
 	defer outputFile.Close()
 
