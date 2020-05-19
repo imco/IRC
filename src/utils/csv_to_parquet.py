3795a333aeb9867906fabe2a8b2a4f174301e768
Carranza <fatcarranza@protonmail.com>
2020-05-18 21:01:40 -0700

    Update csv_to_parquet to work for participantes.psv

--- src/utils/csv_to_parquet.py
+++ src/utils/csv_to_parquet.py
@@ -1,6 +1,7 @@
 # csv_to_parquet.py <csv_file>
 # adaptado de https://stackoverflow.com/questions/26124417/how-to-convert-a-csv-file-to-parquet
 
+import numpy as np
 import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
@@ -14,20 +15,37 @@ print("Writing", parquet_file)
 chunksize = 100_000
 
 csv_stream = pd.read_csv(csv_file,
+                         dtype={
+                             "CANTIDAD": float,
+                             "CODIGO_EXPEDIENTE": float,
+                             "PRECIO_TOTAL": float,
+                             "PRECIO_UNITARIO": float
+                         },
                          sep='|',
-                         quoting=1,
                          chunksize=chunksize,
                          low_memory=False)
 
 for i, chunk in enumerate(csv_stream):
     print("Chunk", i)
+
+    # Cast CODIGO_EXPEDIENTE as integer
+    # We had to use float at the beginning so we could parse nan's
+    chunk["CODIGO_EXPEDIENTE"] = chunk["CODIGO_EXPEDIENTE"].fillna(-1)
+    chunk["CODIGO_EXPEDIENTE"] = chunk["CODIGO_EXPEDIENTE"].apply(pd.to_numeric, downcast='integer')
+
     if i == 0:
         # Guess the schema of the CSV file from the first chunk
-        chunk = chunk.astype({"IDENTIFICADOR_CM": str})
+        # To make it work for procedimientos.psv
+        # chunk = chunk.astype({"IDENTIFICADOR_CM": str})
+        print('SCHEMA chunk')
+        print(chunk.dtypes)
+        print('------')
+
         parquet_schema = pa.Table.from_pandas(df=chunk).schema
-        print('SCHEMA')
+        print('SCHEMA parquet')
         print(parquet_schema)
         print('------')
+
         # Open a Parquet file for writing
         parquet_writer = pq.ParquetWriter(parquet_file, parquet_schema, compression='snappy')
     # Write CSV chunk to the parquet file
@@ -35,7 +53,8 @@ for i, chunk in enumerate(csv_stream):
         table = pa.Table.from_pandas(chunk, schema=parquet_schema)
         parquet_writer.write_table(table)
     except:
-        print('FAILURE\n', chunk)
+        failed_column = 'CANTIDAD'
+        print('FAILURE\n', chunk[failed_column])
         raise
 
 parquet_writer.close()
