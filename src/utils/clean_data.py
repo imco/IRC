2746f0c5776525c4c411060a2d00549d37f3f095
Carranza <fatcarranza@protonmail.com>
2020-05-26 22:22:59 -0700

    Refactor procedimientos ETL so it can be reused for 2018

--- src/utils/clean_data.py
+++ src/utils/clean_data.py
@@ -1,6 +1,8 @@
-
+import os
 import re
 import pandas as pd
+from currency_converter import CurrencyConverter
+from joblib import Parallel, delayed
 from typing import Set, List
 
 DataFrame = pd.DataFrame
@@ -224,3 +226,178 @@ def clean_base_sancionados(df: DataFrame) -> DataFrame:
         )
     df = df.assign(PROVEEDOR_CONTRATISTA=df.PROVEEDOR_CONTRATISTA.str.strip())
     return df
+
+
+def ensamblar_procedimientos(files: List[str],
+                             years: List[int],
+                             base_path: str = '.') -> DataFrame:
+    """
+    Toma una lista de procedimientos de contratación de Compranet
+    y los convierte en un DataFrame, aplicando varias transformaciones.
+    """
+    columnas_procs: List[str] = [
+        'GOBIERNO',
+        # Dependencia/unidad/responsable
+        'DEPENDENCIA',
+        'SIGLAS',
+        'NOMBRE_DE_LA_UC',
+        'CLAVEUC',
+        'RESPONSABLE',
+        'ESTRATIFICACION_MUC',
+        # proveedor
+        'FOLIO_RUPC',
+        'PROVEEDOR_CONTRATISTA',
+        'ESTATUS_EMPRESA',
+        'ESTRATIFICACION_MPC',
+        'CLAVE_CARTERA_SHCP',
+        # montos
+        'IMPORTE_CONTRATO',
+        'APORTACION_FEDERAL',
+        'MONEDA',
+        # datos procedimiento
+        'NUMERO_PROCEDIMIENTO',
+        'FORMA_PROCEDIMIENTO',
+        'TIPO_PROCEDIMIENTO',
+        # datos contrato
+        'CODIGO_CONTRATO',
+        'TITULO_CONTRATO',
+        'IDENTIFICADOR_CM',
+        'TIPO_CONTRATACION',
+        'ESTATUS_CONTRATO',
+        'COMPRA_CONSOLIDADA',
+        'PLURIANUAL',
+        'CARACTER',
+        'CONTRATO_MARCO',
+        'CONVENIO_MODIFICATORIO',
+        # Fechas ordenadas
+        'PROC_F_PUBLICACION',
+        'FECHA_APERTURA_PROPOSICIONES',
+        'EXP_F_FALLO',
+        'FECHA_CELEBRACION',
+        'FECHA_INICIO',
+        'FECHA_FIN',
+        # expediente
+        'CODIGO_EXPEDIENTE',
+        'TITULO_EXPEDIENTE',
+        'PLANTILLA_EXPEDIENTE',
+        'CLAVE_PROGRAMA',
+        'CUENTA_ADMINISTRADA_POR',
+        'ANUNCIO',
+        'ARCHIVADO',
+        'SIGLAS_PAIS',
+        'RAMO',
+        'ORGANISMO',
+        'C_EXTERNO',
+        'FECHA_ARCHIVO',
+    ]
+
+    converter_pesos = CurrencyConverter(fallback_on_missing_rate=True,
+                                        verbose=False,
+                                        fallback_on_wrong_date=True)
+
+    directorio_unidades_path = os.path.join(base_path, 'directorio_UC.xlsx')
+    exped_repeditos_path = os.path.join(base_path, 'codigos_expediente_repetidos.csv')
+
+    dfs = Parallel(n_jobs=-1, batch_size=1)(
+        delayed(procesar_archivo_procedimientos)(os.path.join(base_path, file), year)
+        for file, year in zip(files, years)
+    )
+    df_procs: DataFrame = pd.concat(dfs, axis=0, ignore_index=True)
+    print(df_procs.shape)
+    df_procs = df_procs.loc[:, columnas_procs]
+    print(df_procs.shape)
+    # solo procedimientos de 2012 en adelante
+    df_procs = df_procs.loc[(df_procs.FECHA_INICIO.dt.year > 2011), :]
+    # Se reemplazan los valores de algunas dependencias
+    dependencias_repetidas = {
+        'HOSPITAL GENERAL DE MEXICO "DR. EDUARDO LICEAGA"': 'HOSPITAL GENERAL DE MEXICO',
+        'INSTITUTO NACIONAL DE REHABILITACION LUIS GUILLERMO IBARRA IBARRA': 'INSTITUTO NACIONAL DE REHABILITACION',
+        'CENTRO DE INVESTIGACION EN GEOGRAFIA Y GEOMATICA, "ING. JORGE L. TAMAYO", A.C.': 'CENTRO DE INVESTIGACION EN GEOGRAFIA Y GEOMATICA "ING. JORGE L. TAMAYO", A.C.',
+        'TRIBUNAL FEDERAL DE JUSTICIA FISCAL Y ADMINISTRATIVA': 'TRIBUNAL FEDERAL DE JUSTICIA ADMINISTRATIVA',
+        'INSTITUTO DE INVESTIGACIONES ELECTRICAS': 'INSTITUTO NACIONAL DE ELECTRICIDAD Y ENERGIAS LIMPIAS'
+    }
+    for k, v in dependencias_repetidas.items():
+        df_procs.loc[df_procs.DEPENDENCIA == k, 'DEPENDENCIA'] = v
+    del dependencias_repetidas
+    # Se intenta rescatar las claves de uc de los nombres y los nums de proc
+    df_procs = df_procs.assign(
+        CLAVEUC_PROC=df_procs.NUMERO_PROCEDIMIENTO.map(get_claveuc_proc),
+        CLAVEUC_NOM=df_procs.NOMBRE_DE_LA_UC.map(get_claveuc_nombre)
+    )
+    # Se completan algunas CLAVEUC
+    claves_con_pad = df_procs.CLAVEUC.str.pad(9, fillchar='0')
+    df_procs.loc[df_procs.CLAVEUC.str.len() == 8, 'CLAVEUC'] = claves_con_pad
+    df_procs.loc[df_procs.CLAVEUC.str.len() == 7, 'CLAVEUC'] = claves_con_pad
+
+    # Leer uc oficiales
+    directorio_unidades_compradoras = pd.read_excel(
+        directorio_unidades_path, dtype={'CLAVE_UC': str})
+    directorio_unidades_compradoras = set(
+        directorio_unidades_compradoras.CLAVE_UC.unique())
+
+    df_procs = get_claveuc_real(df_procs, directorio_unidades_compradoras)
+    df_procs = (df_procs.assign(CLAVEUC=df_procs.CLAVEUC_REAL)
+                        .drop('CLAVEUC_REAL', axis=1))
+    # convertir import en pesos mexicanos
+    # Transform to mexican pesos
+    monto_en_pesos = convert_to_mxn(
+        converter_pesos, df_procs.IMPORTE_CONTRATO.values,
+        df_procs.MONEDA.values, df_procs.FECHA_INICIO
+    )
+
+    df_procs = df_procs.assign(IMPORTE_PESOS=monto_en_pesos)
+    # Llenar el valor de convenio cuando sea Nan con 'No reportado'
+    # llenar convenio modificatorio
+    df_procs = df_procs.assign(
+        CONVENIO_MODIFICATORIO=df_procs.CONVENIO_MODIFICATORIO.astype(str)
+    )
+    df_procs.loc[
+        df_procs.CONVENIO_MODIFICATORIO == '0.0', 'CONVENIO_MODIFICATORIO'
+    ] = 'NO'
+    df_procs.loc[
+        df_procs.CONVENIO_MODIFICATORIO == '1.0', 'CONVENIO_MODIFICATORIO'
+    ] = 'SI'
+    df_procs.loc[
+        df_procs.CONVENIO_MODIFICATORIO == 'nan', 'CONVENIO_MODIFICATORIO'
+    ] = 'NO REPORTADO'
+
+    # se reemplaza el valor del tipo de procedimiento para que sea
+    #  consistente con el resto de las tablas
+    df_procs.loc[
+        df_procs.TIPO_PROCEDIMIENTO == 'INVITACION A CUANDO MENOS 3 PERSONAS',
+        'TIPO_PROCEDIMIENTO'
+    ] = 'INVITACION A CUANDO MENOS TRES'
+    df_procs.loc[
+        df_procs.TIPO_PROCEDIMIENTO == 'ADJUDICACION DIRECTA FEDERAL',
+        'TIPO_PROCEDIMIENTO'
+    ] = 'ADJUDICACION DIRECTA'
+
+    # Remover codigos de expedientes que se repiten
+    codigos_exped_repeditos = pd.read_csv(
+        exped_repeditos_path, dtype=str,
+        usecols=['CODIGO_EXPEDIENTE']
+    )
+    codigos_exped_repeditos = codigos_exped_repeditos.CODIGO_EXPEDIENTE.unique()
+    codigos_exped_repeditos = set(codigos_exped_repeditos)
+    df_procs = df_procs.assign(codigos_repetidos=0)
+    df_procs.loc[
+        df_procs.CODIGO_EXPEDIENTE.isin(codigos_exped_repeditos),
+        'codigos_repetidos'] = 1
+    cols = [
+        'CLAVEUC', 'NUMERO_PROCEDIMIENTO', 'CODIGO_EXPEDIENTE',
+        'CODIGO_CONTRATO', 'codigos_repetidos', 'PROVEEDOR_CONTRATISTA']
+    # Se remueven cerca de 550 registros
+    df_procs = df_procs.drop_duplicates(subset=cols)
+    df_procs = df_procs.drop('codigos_repetidos', axis=1)
+
+    # Se quita a pemex del analisis
+    pemex_cond = df_procs.DEPENDENCIA.str.contains('PEMEX')
+    df_procs = df_procs.loc[~pemex_cond]
+    pemex_cond = df_procs.DEPENDENCIA.str.contains('PETROLEOS MEXICANOS')
+    df_procs = df_procs.loc[~pemex_cond]
+    # guardar el dataframe
+    drop_cols = ['CLAVE_CARTERA_SHCP', 'CLAVEUC_PROC', 'CLAVEUC_NOM']
+    df_procs = df_procs.drop(drop_cols, axis=1)
+    df_procs = df_procs.reset_index(drop=True)
+
+    return df_procs
