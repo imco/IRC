782b3262b30ef381659afe75cebf7134fe91ee3d
Carranza <fatcarranza@protonmail.com>
2020-07-06 17:15:09 -0700

    Add first unit test for transparencia.py
    Covers new feature: pc_procs_con_testigo_social

---
+++ test/test_transparencia.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+from features.transparencia import (
+    pc_procs_con_testigo_social
+)
+
+
+class TestAnomalias:
+    def test_pc_procs_con_testigo_social(self):
+        df_test = pd.DataFrame(data=[
+            ['001', 1000, 1],
+            ['001', 1001, 1],
+            ['001', 1002, 0],
+            ['002', 1003, 0],
+            ['002', 1004, 0],
+            ['002', 1005, 0]
+        ], columns=['CLAVEUC', 'CODIGO_EXPEDIENTE', 'testigo_social'])
+
+        res = pc_procs_con_testigo_social(df_test)
+        assert(res.iloc[0].pc_procs_con_testigo_social > 66.6)
+        assert(res.iloc[1].pc_procs_con_testigo_social == 0)
